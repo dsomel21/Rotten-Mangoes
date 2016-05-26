c1968128ba1fdc593dd0b5d11188fbc8c451b286
dsomel21 <dsomel21@gmail.com>
2016-05-26 13:24:22 -0400

    Got user to delete with admin/users#destory action, and undid accidental commit on master

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
 	default from: "from@example.com"
-	# layout 'mailer'
-	def account_suspend_email(user)
-		@user = user
-		mail(to: @user.email, subject: 'Your account has been suspended'
-	end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,5 @@
 class UserMailer < ApplicationMailer
-
+	def suspend_email(user)@user = user
+		mail(to: @user.email, subject: 'Account Suspension'
+	end
 end
